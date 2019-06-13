ee0d032bb7c1c07ccbbe8c609ffc0d78dba00e74
Mandeep Mehra <mandeep.mehra@xebia.com>
2019-06-13 14:12:03 +0530

    Fixed default context issue

--- vars/scanImageUT.groovy
+++ vars/scanImageUT.groovy
@@ -2,7 +2,7 @@ import com.xebia.ioc.ContextRegistry
 import com.xebia.scanner.DockerImageScanner
 
 def call(String imageNameWithPath, String scannerClientIP, String scannerHost, String scannerPort){
-    ContextRegistry.registerContext(this)
+    ContextRegistry.registerDefaultContext(this)
 
     def scanner = new DockerImageScanner(imageNameWithPath, scannerClientIP, scannerHost, scannerPort)
     scanner.scan()
