cbc8cbf587949b7b8e7d733cff8ad4cb2e16b599
Mandeep Mehra <mandeep.mehra@xebia.com>
2019-06-13 13:41:34 +0530

    Fixed signature in groovy for call method

--- vars/scanImageUT.groovy
+++ vars/scanImageUT.groovy
@@ -1,9 +1,9 @@
 import com.xebia.ioc.ContextRegistry
 import com.xebia.scanner.DockerImageScanner
 
-def call(String imageNameWithPath){
+def call(String imageNameWithPath, String scannerClientIP, String scannerHost, int scannerPort){
     ContextRegistry.registerContext(this)
 
-    def scanner = new DockerImageScanner(imageNameWithPath)
+    def scanner = new DockerImageScanner(imageNameWithPath, scannerClientIP, scannerHost, scannerPort)
     scanner.scan()
 }
